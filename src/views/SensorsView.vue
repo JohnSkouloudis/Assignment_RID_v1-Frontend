a0b87d068406e0c0fed71fbe6421cf3b3f574e01
iskoulou <johnskoul56@gmail.com>
2024-08-12 15:40:03 +0300

    added views

--- src/views/SensorsView.vue
+++ src/views/SensorsView.vue
@@ -1,9 +1,31 @@
 <script setup>
+import { ref } from 'vue'
 
 </script>
 
 <template>
+<table>
+  <tr>
+    <th>Id</th>
+    <th>type</th>
+    <th>vendorName</th>
+    <th>vendorEmail</th>
+    <th>description</th>
+    <th>location</th>
 
+  </tr>
+  <tr v-for="sensor in sensors" :key="sensor.id">
+    <td>{{ sensor.id }}</td>
+    <td>{{ sensor.sensortype }}</td>
+    <td>{{ sensor.vendorName }}</td>
+    <td>{{ sensor.vendorEmail }}</td>
+    <td>{{ sensor.description }}</td>
+    <td>{{ sensor.location }}</td>
+    <RouterLink to="/SensorMetrics/{{ sensor.id }}">Metrics</RouterLink>
+    <RouterLink to="/Readings/{{ sensor.id }}">Readings</RouterLink>
+
+  </tr>
+</table>
 </template>
 
 <style scoped>
