3ced63aadc3516708d931752c129ba2145edefbf
iskoulou <johnskoul56@gmail.com>
2024-08-12 13:00:04 +0300

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
+
+<template>
+
+
+ <header>
+   <ul>
+     <li>
+        <RouterLink to="/">Home</RouterLink>
+     </li>
+     <li>
+        <RouterLink to="/sensors">Sensors</RouterLink>
+     </li>
+     <li>
+        <RouterLink to="/search">Search</RouterLink>
+     </li>
+   </ul>
+ </header>
+
+</template>
+
+<style scoped>
+
+</style>
