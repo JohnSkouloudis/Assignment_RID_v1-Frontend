ea336d988ce356bf6cbdec0d69c1bdfd6198f4f2
John skoul <johnskoul56@gmail.com>
2024-08-24 16:34:42 +0300

    added icons for homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,44 @@
 </script>
 
 <template>
-  <main>
-   Home Page
-  </main>
+
+  <h2> Welcome to the Sensor Management System </h2>
+  <p>This is the user interface for managing and viewing sensors and their data </p>
+
+  <h3>About the Frontend</h3>
+  <p>
+    The Frontend was built using VueJS and Bootstrap and it was designed to interact with the Backend
+    and to provide a simple interface for the user to view and manage sensors
+  </p>
+  <h4>Types of Sensors</h4>
+  <ul>
+    <li>
+      <img src="/temperature-control.png" alt="Temperature Icon" style="width: 30px; height: 30px; margin-right: 5px;">
+      Temperature Sensor
+    </li>
+    <li>
+      <img src="/humidity.png" alt="Humidity Icon" style="width: 30px; height: 30px; margin-right: 5px;">
+      Humidity Sensor
+    </li>
+    <li>
+      <img src="/sensor-alert.png" alt="Humidity Icon" style="width: 25px; height: 25px; margin-right: 5px;">
+      Acoustic Sensor
+    </li>
+  </ul>
+
+  <h3>About the Backend</h3>
+  <p></p>
+
+  <h3>Repositories</h3>
+  <ul>
+    <li>
+       <a href='https://github.com/JohnSkouloudis/Assignment_RID_v1' target='_blank'><img src="/github-mark.png" alt='github'  style="width: 35px; height: 35px;"></a>
+      Backend
+    </li>
+    <li>
+      <a href='https://github.com/JohnSkouloudis/Assignment_RID_v1_Frontend' target='_blank' ><img src="/github-mark-blue.png" alt='github'  style="width: 35px; height: 35px; "></a>
+      Frontend
+    </li>
+  </ul>
+
 </template>
